8c5c2914c9746229f4f1935e172a856691b3a9f3
Refiloe Mopeloa <72868391+refiloemopeloa@users.noreply.github.com>
2025-05-10 19:28:58 +0200

    Created hello.cpp.
    - Added code to take user name.

---
+++ hello-world/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string name;
+    
+    cout << "What's your name?\n";
+    cin >> name;
+
+    cout<< "Hello, " << name << "!" <<  endl;
+
+    return 0;
+}
